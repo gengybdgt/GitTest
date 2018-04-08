bb29a2d2f0ecd6049198484b3fc36db0bb42e51e
gengybdgt <gengyb@dgtis.com>
2018-04-08 20:50:38 +0800

    git 测试

---
+++ src/com/gyb/User.java
@@ -0,0 +1,23 @@
+package com.gyb;
+
+public class User {
+
+    private String username;
+    private Strign password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Strign getPassword() {
+        return password;
+    }
+
+    public void setPassword(Strign password) {
+        this.password = password;
+    }
+}
